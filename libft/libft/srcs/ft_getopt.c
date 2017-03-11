288ef6202c7413601643ea1234cd3b701c292d63
Laurent MEYER <lmeyer@e1r9p10.42.fr>
2017-03-11 23:02:23 +0100

    amelioration reflexion algo

--- libft/libft/srcs/ft_getopt.c
+++ libft/libft/srcs/ft_getopt.c
@@ -6,11 +6,12 @@
 /*   By: lmeyer <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/02/03 12:34:00 by lmeyer            #+#    #+#             */
-/*   Updated: 2017/02/27 16:30:19 by lmeyer           ###   ########.fr       */
+/*   Updated: 2017/03/11 23:01:29 by lmeyer           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <unistd.h>
+#include "libft.h"
 #define UNKNOWN 0
 #define MISSING_ARG 1
 
@@ -20,13 +21,17 @@ int		g_optopt = '\0';
 int		g_opterr = 1;
 int		g_optreset = 0;
 
-static void	st_error(int errcode)
+static int	st_error(int errcode)
 {
+	(void)errcode;		//
 	write(2, "erreur", 7);
+	return (-1);
 }
 
 int			ft_getopt(int argc, const char **argv, const char *optstring)
 {
+	(void)argc;			// ca sent pas bon
+
 	if (g_optreset == 1 && !(g_optopt = '\0'))
 		g_optreset = 0; // vraiment pas sur pour cette partie la
 	if (argv[g_optind] == NULL || argv[g_optind][0] != '-' ||
@@ -34,18 +39,18 @@ int			ft_getopt(int argc, const char **argv, const char *optstring)
 		return (-1);
 	g_optopt = (g_optopt == '\0') ? argv[g_optind][1]
 		: ft_strchr(argv[g_optind], g_optopt)[1];
-	if (!ft_strchr(g_optstring, g_optopt))
+	if (!ft_strchr(optstring, g_optopt))
 		return (st_error(UNKNOWN));
-	if (ft_strchr(g_optstring, g_optopt)[1] == ':')
+	if (ft_strchr(optstring, g_optopt)[1] == ':')
 	{
 		if ((*(g_optarg = ft_strchr(argv[g_optind++], g_optopt) + 1) != '\0' ||
-					((g_optarg = argv[g_optind++]) != NULL
+					((g_optarg = (char *)argv[g_optind++]) != NULL
 					 && ft_strcmp(g_optarg, "--"))) && (g_optreset = 1))
 			return (g_optopt);
 		g_optarg = NULL;
 		return (st_error(MISSING_ARG));
 	}
-	if (ft_strchr(g_optstring, g_optopt)[1] == '\0' && (g_optreset = 1))
+	if (ft_strchr(optstring, g_optopt)[1] == '\0' && (g_optreset = 1))
 		g_optind++;
 	return (g_optopt);
 }
--- test_create.sh
+++ test_create.sh
@@ -1,12 +1,20 @@
 #!/bin/bash
 
+cd ~/ft_ls
+chmod 0777 test/dirnorights
+rm -rf test
+
 mkdir test
 cd test
 touch aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa
 touch rootfile
+mkfifo fifo1
 echo "file1 content" > rootfile
 mkdir dir1
 ln -s rootfile1 dir1/symlink1
+mkdir dir1/subdir1
+touch dir1/subdir1/file1
+echo "file1 content" > dir1/subdir1/file1
 mkdir dir01
 echo "file1 content" > dir1/file1
 touch dir1/file1
@@ -17,4 +25,8 @@ echo "file1 content" > dir2/file1
 mkdir dir11
 touch dir11/file1
 echo "file1 content" > dir11/file1
+mkdir dirnorights
+touch  dirnorights/file1
+echo "file1 content" >  dirnorights/file1
+chmod 0000 dirnorights
 cd ..
