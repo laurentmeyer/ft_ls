81977752da7919a95d261b2d6f9551d295746782
Laurent MEYER <laurent.dubois.meyer@gmail.com>
2017-10-11 15:15:05 +0200

    added color display for special files and folders

--- srcs/display.c
+++ srcs/display.c
@@ -19,10 +19,23 @@ void		display_simple(t_list *children, t_options *options)
 	while (children)
 	{
 		filename = ft_basename(((t_file *)(children->content))->path);
-		if (options->display_dots
-				|| (!ft_strequ(filename, ".")
-					&& !ft_strequ(filename, "..")))
-			ft_printf("%s\n", filename);
+		if (options->display_dots || *filename != '.')
+		{
+			if (S_ISDIR(((t_file *)(children->content))->stat.st_mode))
+				ft_printf(C_BOLD "%s" C_RESET "\n", filename);
+			else if (S_ISFIFO(((t_file *)(children->content))->stat.st_mode))
+				ft_printf(CF_YEL "%s" C_RESET "\n", filename);
+			else if (S_ISCHR(((t_file *)(children->content))->stat.st_mode))
+				ft_printf(CF_BLU CB_YEL "%s" C_RESET "\n", filename);
+			else if (S_ISBLK(((t_file *)(children->content))->stat.st_mode))
+				ft_printf(CF_BLU CB_CYA  "%s" C_RESET "\n", filename);
+			else if (S_ISLNK(((t_file *)(children->content))->stat.st_mode))
+				ft_printf(CF_MAG "%s" C_RESET "\n", filename);
+			else if (S_IXUSR & ((t_file *)(children->content))->stat.st_mode)
+				ft_printf(CF_RED "%s" C_RESET "\n", filename);
+			else
+				ft_printf("%s\n", filename);
+		}
 		children = children->next;
 	}
 }
@@ -40,7 +53,8 @@ void		display_parent_and_children(t_file *parent, t_list *children,
 		ft_printf("\n%s:\n", parent->path);
 	options->display_headers = 1;
 	if (parent->error)
-		ft_dprintf(STDERR, "ls: %s: %s\n", ft_basename(parent->path), parent->error);
+		ft_dprintf(STDERR, "ls: %s: %s\n", ft_basename(parent->path),
+				parent->error);
 	else
 		display_children(children, options);
 }
