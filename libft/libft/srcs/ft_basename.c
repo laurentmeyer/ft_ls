773503856aa1c4823bf79e1e8f05184a7c125f6f
Laurent MEYER <laurent.dubois.meyer@gmail.com>
2017-10-14 15:30:06 +0200

    fixed the basename function

--- libft/libft/srcs/ft_basename.c
+++ libft/libft/srcs/ft_basename.c
@@ -6,7 +6,7 @@
 /*   By: lmeyer <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/09/07 14:35:36 by lmeyer            #+#    #+#             */
-/*   Updated: 2017/09/07 15:11:33 by lmeyer           ###   ########.fr       */
+/*   Updated: 2017/10/14 14:41:42 by lmeyer           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,7 +22,7 @@ char	*ft_basename(char *path)
 	while (path[--i] == '/')
 		if (i == 0)
 			return ("/");
-	while (path[--i] != '/')
-		;
+	while (path[i] != '/' && i >= 0)
+		--i;
 	return (path + i + 1);
 }
